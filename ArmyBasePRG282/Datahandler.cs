182c3e495a778e8d9a20ae497133efee217cba66
Werner Els <wernerels154@gmail.com>
2019-03-05 18:17:05 +0200

    Deleted Conn.Open() line

--- ArmyBasePRG282/Datahandler.cs
+++ ArmyBasePRG282/Datahandler.cs
@@ -24,7 +24,7 @@ namespace ArmyBasePRG282
             SqlDataReader rdr = null;
             try
             {
-                conn.Open();
+               
                rdr = cmd.ExecuteReader();
                 while (rdr.Read())
                 {
